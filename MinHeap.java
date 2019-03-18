ad96ea56e76498b7affd726e29f3cf1a39e299e7
Hunter <huntercavers@xtra.co.nz>
2019-03-18 16:15:19 +1300

    First commit of minHeap class

---
+++ MinHeap.java
@@ -0,0 +1,15 @@
+class MinHeap{
+	private String[] heap;
+	public MinHeap(int arrayLength){
+		heap = new String[arrayLength];
+		heap[0] = 1;
+	}
+	
+	public void Insert(String input){} // Uses up heap to keep sorted
+	
+	public void Pop(){} // Uses  down heap to keep sorted
+	
+	private void DownHeap(){}
+	
+	private void UpHeap(){}
+}
