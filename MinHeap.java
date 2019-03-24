064d631eff32357a058688f3a9c42722a5540230
Hunter <huntercavers@xtra.co.nz>
2019-03-25 11:24:24 +1300

    Added methods to get children and parents in minheap

--- MinHeap.java
+++ MinHeap.java
@@ -5,25 +5,48 @@ class MinHeap{
 	private String[] heap;
 	public MinHeap(int arrayLength){
 		heap = new String[arrayLength];
-		heap[0] = 1;
+		heap[0] = "1";
 	}
 	
-	public void Insert(String input){ // Uses up heap to keep sorted
+	public void Insert(String input)
+	{ // Uses up heap to keep sorted
 		int index = Integer.parseInt(heap[0]);
 		int newIndex = index++;
-		if(newIndex < heap.length()){
+		if(newIndex < heap.length){
 			heap[newIndex] = input;
 		}
 		UpHeap();
 	} 
 	
-	public String Remove(){ // Uses  down heap to keep sorted
+	public String Remove()
+	{ // Uses  down heap to keep sorted
 		int index = Integer.parseInt(heap[0]);
 		String result = heap[1];
-		
+		return "";
 	} 
 	
-	private void DownHeap(){}
+	private void DownHeap()
+	{
+		
+	}
 	
-	private void UpHeap(){}
+	private void UpHeap()
+	{
+		
+	}
+	
+	public int GetLeftChild(int parentID)
+	{
+		return parentID * 2;
+	}
+	
+	public int GetRightChild(int parentID)
+	{
+		return (parentID * 2) + 1;
+	}
+	
+	public int GetParent(int childID)
+	{
+		return childID / 2;
+	}
 }
