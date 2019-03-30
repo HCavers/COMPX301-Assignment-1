8a6f70e75b472c234774907f3a1a8377a10bf39d
Hunter <huntercavers@xtra.co.nz>
2019-03-30 16:47:46 +1300

    Documentation done for file output

--- FileOutput.java
+++ FileOutput.java
@@ -1,6 +1,12 @@
+// Hunter Cavers (1288108)
+// Sivaram Manoharan (1299026)
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+
+// This class was made to be used by the file manager class to 
+// handle writing output to a file
 
 class FileOutput
 {
@@ -13,8 +19,10 @@ class FileOutput
 		firstWrite = true;
 	}
 	
+	// Writes a line to the file
 	public void writeLine(String input) throws IOException
 	{
+		// If this is the first input that is being written don't write newline;
 		if(firstWrite == false)
 		{
 			writer.newLine();
@@ -27,6 +35,7 @@ class FileOutput
 		writer.flush();
 	}
 	
+	// Close the writer to the file
 	public void close() throws IOException
 	{
 		writer.close();
