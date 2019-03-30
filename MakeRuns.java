1b8452ef23cb2f6f78b6decaf9497861a9b73edf
maravis <sivaramigcse@gmail.com>
2019-03-30 13:00:15 +1300

    Removed old code, now prints out numRuns and output file name

--- MakeRuns.java
+++ MakeRuns.java
@@ -11,39 +11,22 @@ public class MakeRuns {
 
         int size = Integer.parseInt(args[0]);
         File Input = new File (args[1]);
-
-        System.out.println(args[1]);
-        //int NumLines = LineCount(filePath);
-        //String[] LineList= new String[NumLines];
+        int numRuns = 1;
 
         //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader br = new BufferedReader(new FileReader(Input));
         MinHeap PriorityQ = new MinHeap(size);
-        String Line = br.readLine();
-        System.out.println(Line);
-        PriorityQ.Push(Line);
-        //PriorityQ.Print();
+        String Line = "";
         File Output = new File("Output.txt");
         BufferedWriter bw = new BufferedWriter(new FileWriter(Output));
         while(Line != null){
-            for(int i = 2; i < size ; i++){ // phase 1 -> Fill up the heap
+            for(int i = 1; i < size ; i++){ // phase 1 -> Fill up the heap
                 Line = br.readLine();
-                System.out.println(Line);
                 PriorityQ.Push(Line);
-
-
             }
-            System.out.println("Heap is filled up");
-            PriorityQ.Print(); // For testing
             String lastOutput = PriorityQ.Next();
-          //  bw.write(lastOutput);
-          //  bw.newLine();
-            // Write to output
             while(PriorityQ.Next() != null){ // While the heap is not empty
                 if(IsGreater(PriorityQ.Next(),lastOutput)){ // if the next item is larger than last output
-                    // Write to output
-                    //System.out.println("Is Greater");
-                    //System.out.println(PriorityQ.Next());
                     bw.write(PriorityQ.Next());
                     bw.newLine();
                     lastOutput = PriorityQ.Next();
@@ -54,29 +37,22 @@ public class MakeRuns {
                     else{
                         PriorityQ.RemoveRoot();
                     }
-
-
                 }
                 else{
-                    System.out.println("Block");
                     PriorityQ.Block();
                 }
 
                 if(PriorityQ.ReservedSpace() == 1){
-                    // min heap is blocked -> Write seperator character to output
-                    // bw.write(Character.toString((char)29));
-                    bw.write("END OF RUN");
+                    numRuns++;
+                    bw.write(Character.toString((char)29));
                     bw.newLine();
-                    //bw.write("End OF RUN");
-                    System.out.println("Reset");
                     PriorityQ.Reset();
                 }
             }
-            System.out.println("Finished Run");
         }
         if(PriorityQ.ReservedSpace() < size){
+            numRuns++;
             bw.write(Character.toString((char)29));
-            //bw.write("END OF RUN");
             bw.newLine();
             PriorityQ.Reset();
             while(PriorityQ.Next() != null){
@@ -84,26 +60,10 @@ public class MakeRuns {
                 PriorityQ.RemoveRoot();
             }
         }
+        System.out.println(numRuns);
+        System.out.println(Output);
         bw.close();
         br.close();
-
-
-        // Old Code
-        //String currLine = br.readLine();
-        //int count = 0;
-        // while (currLine != null){
-        //     LineList[count] = currLine;
-        //     count++;
-        //     currLine = br.readLine();
-        //}
-        // int TotalRuns = NumRuns(NumLines,size);
-        // System.out.println(TotalRuns);
-        // System.out.println(Arrays.toString(LineList)); // For Testing
-        // String OutputFilePath = filePath.substring(0, filePath.lastIndexOf("\\")) + "\\" + "Output.txt";
-        // System.out.println(OutputFilePath);
-        // GenerateRuns(OutputFilePath,TotalRuns,size,LineList);
-
-
     }
 
     public static boolean IsLower (String First, String Second){ // Returns true if first is lower than second
@@ -117,62 +77,4 @@ public class MakeRuns {
     public static boolean IsGreater (String First, String Second){
         return IsLower(Second, First);
     }
-
-    public static int LineCount(String Path) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String currLine = br.readLine();
-        int i = 0;
-        while (currLine != null){
-            i++;
-            currLine = br.readLine();
-        }
-        br.close();
-        return i;
-    }
-
-    public static int NumRuns(int NumList, int HeapSize){
-        if(NumList % HeapSize == 0){
-            return NumList / HeapSize;
-        }
-
-        else{
-            return (NumList / HeapSize) + 1;
-        }
-    }
-
-    public static String[] CreateRun(int startIndex, int heapsize, String[] Lines){
-        String[] CurrRun = new String[heapsize];
-        int currIndex = 0;
-        while(startIndex + currIndex < Lines.length && currIndex < heapsize){
-            CurrRun[currIndex] = Lines[startIndex + currIndex];
-            //System.out.println("Currindex is: " + currIndex);
-            //System.out.println("startindex is: " +  startIndex);
-            currIndex++;
-        }
-        //System.out.println("Ended Run");
-        System.out.println(Arrays.toString(CurrRun));
-        return CurrRun;
-    }
-
-    public static void WriteRun(BufferedWriter bw, int startIndex, int heapsize, String[] Lines)  throws IOException{
-        String[] Run = CreateRun(startIndex,heapsize,Lines);
-        bw.write(Arrays.toString(Run));
-
-    }
-
-    public static void GenerateRuns(String filepath, int numRuns,int heapsize,String[] Lines)throws IOException{
-        int startIndex = 0;
-        int currRuns = 0;
-        File Output = new File(filepath);
-        BufferedWriter bw = new BufferedWriter(new FileWriter(Output));
-        while(currRuns < numRuns){
-            //System.out.println("Writing Run");
-            WriteRun(bw,startIndex,heapsize,Lines);
-            startIndex = startIndex + heapsize;
-            currRuns++;
-        }
-        bw.close();
-
-    }
-
 }
